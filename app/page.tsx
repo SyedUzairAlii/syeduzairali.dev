055280f9a3465a4c8b69ea52e0ba4e9d671ee75b
Syed Uzair Ali <maazuzair3322@gmail.com>
2026-09-04 03:26:04 +0500

    feat: add custom cursor component and update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CustomCursor from "@/components/CustomCursor";
 import IntroSection from "@/components/IntroSection";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#D8E4E7]">
+      <CustomCursor />
       <Navbar />
 
       <IntroSection />
