da10f429367bd9c58def15d3f1cf9e404d2ffab0
Syed Uzair Ali <maazuzair3322@gmail.com>
2026-09-04 06:07:56 +0500

    feat: add Vercel Analytics integration and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,23 @@ import CustomCursor from "@/components/CustomCursor";
 import IntroSection from "@/components/IntroSection";
 import Navbar from "@/components/Navbar";
 import SkillsSection from "@/components/SkillsSection";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#D8E4E7]">
-      <CustomCursor />
+    <>
+      <div className="min-h-screen bg-[#D8E4E7]">
+        <CustomCursor />
 
-      <Navbar />
+        <Navbar />
 
-      <IntroSection />
+        <IntroSection />
 
-      <AboutSection />
+        <AboutSection />
 
-      <SkillsSection />
-    </div>
+        <SkillsSection />
+      </div>
+      <Analytics />
+    </>
   );
 }
